f25bb8bf59ac8f9448255b7e5c957c841e237973
takuyakasagawa <52574278+takuyakasagawa@users.noreply.github.com>
2021-06-03 08:54:56 +0900

    スプレッド構文

--- src/index.js
+++ src/index.js
@@ -80,8 +80,68 @@
 // const func3 = (str) => str;
 // console.log(func3("func3です"));
 
-const func4 = (num1, num2) => {
-  return num1 + num2;
-};
+// const func4 = (num1, num2) => {
+//   return num1 + num2;
+// };
 
-console.log(func4(10, 20));
+// console.log(func4(10, 20));
+
+//分割代入
+
+// const myprofile = {
+//   name: "わに",
+//   age: 108
+// };
+
+// const message1 = `名前は${myprofile.name}です。年齢は${myprofile.age}歳です。`;
+// console.log(message1);
+
+// const { name, age } = myprofile;
+// const message2 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+
+// const myprofile = ["わに", 108];
+
+// const message3 = `私の名前は${myprofile[0]}です。年齢は${myprofile[1]}歳です。`;
+// console.log(message3);
+
+// const [name, age] = myprofile;
+// const message4 = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(message4);
+
+//デフォルト値、引数
+
+// const SayHello = (name = "ゲスト") => console.log(`こんにちは!${name}さん!`);
+// SayHello("わに");
+
+//スプレッド構文
+
+//配列
+// const arr1 = [9, 2];
+
+// console.log(arr1);
+// console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+//まとめる
+// const arr2 = [1,2,3,4,5];
+// const [num1,num2,...arr3] = arr2;
+
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+//配列のコピー、結合
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
+
+// const arr6 = [...arr4];
+// arr6[0] = 100;
+// console.log(arr4);
+// console.log(arr6);
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
